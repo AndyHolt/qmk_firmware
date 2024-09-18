12f52be0c3c62d58bf333338f92b5ea28b05f8c1
Andy Holt <andrew.holt@hotmail.co.uk>
2024-09-18 09:51:42 +0100

    Define tapping term length & disable hold on other key press
    Tapping term configures how long a key needs to be held before it is considered
    a hold rather than a tap. This is a dynamic binding expected to change regularly
    as I seek out the sweet spot for my typing style and familiarity with mod-taps.
    
    Disabling HOLD_ON_OTHER_KEY_PRESS is expected to be a permanent change, which
    allows typing with key rolls on mod-tap keys without accidentally calling the
    hold behaviour. It is typically off by default, but for some reason is enabled
    in the Keychron Q1V2 config file, so needs to be overridden in the keymap
    specific config file.

---
+++ keyboards/keychron/q1v2/iso_encoder/keymaps/AndyHolt/config.h
@@ -0,0 +1,9 @@
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 500
+
+// Enable rapid switch from tap to hold, disabling double-tap-hold auto-repeat
+// Not sure if this is what I will want, but experiment for now
+#define QUICK_TAP_TERM 0
+
+// Override the Q1V2 default of having HOLD_ON_OTHER_KEY_PRESS enabled
+#undef HOLD_ON_OTHER_KEY_PRESS
