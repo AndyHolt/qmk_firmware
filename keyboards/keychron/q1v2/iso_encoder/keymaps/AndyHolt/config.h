ce22f613eec4488dba5350b0fcbbc0a2b81a78db
Andy Holt <andrew.holt@hotmail.co.uk>
2024-10-22 08:56:42 +0100

    Set quick tap term for repeating mod-tap keys

--- keyboards/keychron/q1v2/iso_encoder/keymaps/AndyHolt/config.h
+++ keyboards/keychron/q1v2/iso_encoder/keymaps/AndyHolt/config.h
@@ -3,7 +3,7 @@
 
 // Enable rapid switch from tap to hold, disabling double-tap-hold auto-repeat
 // Not sure if this is what I will want, but experiment for now
-#define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM 120
 
 // Override the Q1V2 default of having HOLD_ON_OTHER_KEY_PRESS enabled
 #undef HOLD_ON_OTHER_KEY_PRESS
