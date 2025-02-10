8d0ab6a38aa506137942acfab705b66aba64464e
Andy Holt <andrew.holt@hotmail.co.uk>
2025-02-10 10:24:40 +0000

    Add a layer for non-alpha characters in low-reach places
    Bring enter, backspace and tab keys onto the home row, using the spacebar held
    as the layer modifier. Also puts a 'keypad' of numbers under left hand

--- keyboards/keychron/q1v2/iso_encoder/keymaps/AndyHolt/keymap.c
+++ keyboards/keychron/q1v2/iso_encoder/keymaps/AndyHolt/keymap.c
@@ -22,7 +22,8 @@ enum layers{
   MAC_BASE,
   MAC_FN,
   WIN_BASE,
-  WIN_FN
+  WIN_FN,
+  MAC_SYM
 };
 
 #define KC_TASK LGUI(KC_TAB)
@@ -40,6 +41,20 @@ enum layers{
 #define HOME_L LALT_T(KC_L)
 #define HOME_SCLN RGUI_T(KC_SCLN)
 
+// Space bar tap-hold layer modifier
+#define SPACE_LT LT(MAC_SYM, KC_SPC)
+
+// Left-hand home row mods in symbol layer
+#define HOME_4 LGUI_T(KC_4)
+#define HOME_5 LALT_T(KC_5)
+#define HOME_6 LSFT_T(KC_6)
+#define HOME_TAB LCTL_T(KC_TAB)
+
+// Right-hand home row mods in symbol layer
+#define HOME_ENT RCTL_T(KC_ENT)
+#define HOME_BSPC RSFT_T(KC_BSPC)
+#define HOME_DEL LALT_T(KC_DEL)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_iso_83(
         KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,     KC_VOLD,   KC_VOLU,   KC_DEL,             KC_MUTE,
@@ -47,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,        KC_LBRC,   KC_RBRC,                       KC_PGDN,
         KC_CAPS,  HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,   KC_QUOT,   KC_NUHS,   KC_ENT,             KC_HOME,
         KC_LSFT,  KC_GRV,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,      KC_SLSH,              KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LOPT,  KC_LCMD,                                KC_SPC,                       KC_RCMD,  MO(MAC_FN),  KC_RCTL,   KC_LEFT,   KC_DOWN,  KC_RGHT),
+        KC_LCTL,  KC_LOPT,  KC_LCMD,                                SPACE_LT,                       KC_RCMD,  MO(MAC_FN),  KC_RCTL,   KC_LEFT,   KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_iso_83(
         _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,            RGB_TOG,
@@ -63,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,                      KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,    KC_NUHS,  KC_ENT,             KC_HOME,
         KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                SPACE_LT,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_iso_83(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            RGB_TOG,
@@ -71,7 +86,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,    _______,                      _______,
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______)
+        _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______),
+
+    [MAC_SYM] = LAYOUT_iso_83(
+        KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,   KC_MNXT,  KC_MUTE,     KC_VOLD,   KC_VOLU,   KC_DEL,             KC_MUTE,
+        KC_NUBS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,      KC_9,     KC_0,        KC_MINS,   KC_EQL,    KC_BSPC,            KC_PGUP,
+        KC_TAB,   KC_7,     KC_8,     KC_9,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,      KC_O,     KC_P,        KC_LBRC,   KC_RBRC,                       KC_PGDN,
+        KC_CAPS,  HOME_4,   HOME_5,   HOME_6,   HOME_TAB, KC_G,     KC_H,     HOME_ENT, HOME_BSPC, HOME_DEL, HOME_SCLN,   KC_QUOT,   KC_NUHS,   KC_ENT,             KC_HOME,
+        KC_LSFT,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_V,     KC_B,     KC_N,     KC_M,      KC_COMM,  KC_DOT,      KC_SLSH,              KC_RSFT,  KC_UP,
+        KC_LCTL,  KC_LOPT,  KC_LCMD,                                _______,                         KC_RCMD,  MO(MAC_FN),  KC_RCTL,   KC_LEFT,   KC_DOWN,  KC_RGHT)
+
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -79,6 +103,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [MAC_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
     [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [WIN_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)}
+    [WIN_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+    [MAC_SYM]  = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
 };
 #endif // ENCODER_MAP_ENABLE
